f02d9642a7613a6b01df8e6bc035c4f242a60593
jonathanbisp <jonathan.santos@tgcore.com.br>
2021-12-08 21:56:45 -0300

    adicionando uvicorn

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/favicon.ico")
+async def favico():
+    return None
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
